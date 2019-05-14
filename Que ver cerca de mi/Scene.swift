261e2bbcdff65e237d13a3acd5ab685a3c788c54
Marlon Hernandez <marlonhr1990@gmail.com>
2019-05-15 01:49:41 +0200

    init

---
+++ Que ver cerca de mi/Scene.swift
@@ -0,0 +1,27 @@
+//
+//  Scene.swift
+//  Que ver cerca de mi
+//
+//  Created by Forsaken on 14/05/2019.
+//  Copyright © 2019 marlonhr. All rights reserved.
+//
+
+import SpriteKit
+import ARKit
+
+class Scene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // Setup your scene here
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+    }
+}
+
